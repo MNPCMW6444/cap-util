aca3f7570c6420c2dc7156178b72d7e781c4d51a
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-05-11 09:13:13 +0300

    Update deleteIssues.py

--- jira-automation-scripts/deleteIssues.py
+++ jira-automation-scripts/deleteIssues.py
@@ -1,16 +1,23 @@
 from jira import JIRA
 import os
+from dotenv import load_dotenv
 
 # Your JIRA instance URL
 jira_url = "https://caphub.atlassian.net"
-jira_username = os.environ['USERNAME']
-jira_api_token = os.environ['TOKEN']
+
+
+load_dotenv()
+
+
+jira_username = os.getenv('USEREMAIL')
+jira_api_token = os.getenv('TOKEN')
+
 
 # Authenticate with the JIRA REST API
 jira = JIRA(server=jira_url, basic_auth=(jira_username, jira_api_token))
 
 # Issues to be deleted
-issues_to_delete = [f"DEV-{i}" for i in range(60, 80)]
+issues_to_delete = [f"DEV-{i}" for i in range(6, 77)]
 
 # Delete issues
 for issue_key in issues_to_delete:
