5758daad40cd5815fefda1598e4f29d59496d9bf
YoadNatan <yoad12121223@gmail.com>
2023-05-12 02:29:34 +0300

    Update createIssues.py

--- jira-automation-scripts/createIssues.py
+++ jira-automation-scripts/createIssues.py
@@ -1,90 +1,79 @@
+from dotenv import load_dotenv
 from jira import JIRA
 import os
 
+# load .env file
+load_dotenv()
 
-# Replace the following variables with your JIRA instance information and API credentials
-jira_url = 'https://caphub.atlassian.net'
-# Replace with your admin email address
-jira_username = os.environ['USERNAME']
-jira_api_token = os.environ['TOKEN']
-jira = JIRA(server=jira_url, basic_auth=(jira_username, jira_api_token))
+# Get JIRA credentials from environment variables
+user = os.getenv("JIRA_USER")
+api_token = os.getenv("JIRA_API_TOKEN")
+server = "https://caphub.atlassian.net"
 
-# Replace the following variables with your project and team lead information
-project_key = 'DEV'
-team_lead = 'michael@caphub-group.com'
+# Connect to JIRA
+jira_options = {'server': server}
+jira = JIRA(options=jira_options, basic_auth=(user, api_token))
 
-
-tasks = [
-    {
-        'name': 'User Registration',
-        'description': 'As a user, I want to create an account on the platform to access the services.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-1',
-    },
-    {
-        'name': 'Dashboard',
-        'description': 'As a user, I want a dashboard to access funding options, view applications, and track funding request status.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-2',
-    },
-    {
-        'name': 'Application Form',
-        'description': 'As a user, I want an easy-to-use application form to apply for funding.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-3',
-    },
-    {
-        'name': 'Proprietary Algorithm',
-        'description': 'As a user, I want to be matched with the best funding options based on my data and goals.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-4',
-    },
-    {
-        'name': 'Security Measures',
-        'description': 'As a user, I want my information and data to be secure and confidential.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-5',
-    },
-    {
-        'name': 'Strategic Advisory Service',
-        'description': 'As a user, I want personalized guidance and support throughout the funding process.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-6',
-    },
-    {
-        'name': 'Compliance',
-        'description': 'As a user or admin, I want the platform to comply with relevant laws and regulations.',
-        'importance': 'HIGH',
-        'jira_issue': 'DEMO-7',
-    },
-    {
-        'name': 'Branded Company File',
-        'description': 'As a partner, I want a branded and customized company file for potential investments.',
-        'importance': 'MEDIUM',
-        'jira_issue': 'DEMO-8',
-    },
-    {
-        'name': 'Email Templates',
-        'description': 'As an admin, I want email templates for all stages of contact for both VDs and companies.',
-        'importance': 'MEDIUM',
-        'jira_issue': 'DEMO-9',
-    },
-    {
-        'name': 'Dashboard Tips (Nice to have)',
-        'description': 'As a user, I want the dashboard to provide tips for small changes that will increase matching probability.',
-        'importance': 'LOW',
-        'jira_issue': 'DEMO-10',
-    },
-]
+# Define the project key
+project_key = "AP"
 
 
-for task in tasks:
+def create_issue(project_key, summary, description, issue_type, epic_name=None, epic_link=None, parent=None):
     issue_dict = {
         'project': {'key': project_key},
-        'summary': task['name'],
-        'description': task['description'],
-        'issuetype': {'name': 'Task'},
-        'assignee': {'name': team_lead},
+        'summary': summary,
+        'description': description,
+        'issuetype': {'name': issue_type},
     }
-    new_issue = jira.create_issue(fields=issue_dict)
-    print(f'JIRA issue created: {new_issue.key} - {task["name"]}')
+    if parent:
+        issue_dict['parent'] = {'id': parent}
+    if epic_link:
+        # 'customfield_10014' is usually the Epic Link field, but check your instance to be sure
+        issue_dict['customfield_10014'] = epic_link
+    if epic_name and issue_type == "Epic":
+        # 'customfield_10011' is usually the Epic Name field, but check your instance to be sure
+        issue_dict['customfield_10011'] = epic_name
+    return jira.create_issue(issue_dict)
+
+
+epics_data = [
+    {"name": "User Registration",
+        "description": "As a user, I want to create an account on the platform to access the services"},
+    {"name": "User Dashboard", "description": "As a user, I want a dashboard to access funding options, view applications, and track funding request status"},
+    {"name": "Application Form",
+        "description": "As a user, I want an easy-to-use application form to apply for funding"},
+    {"name": "Proprietary Algorithm",
+        "description": "As a user, I want to be matched with the best funding options based on my data and goals"},
+    {"name": "Security Measures",
+        "description": "As a user, I want my information and data to be secure and confidential"},
+    {"name": "Strategic Advisory Service",
+        "description": "As a user, I want personalized guidance and support throughout the funding process"},
+    {"name": "Compliance", "description": "As a user or admin, I want the platform to comply with relevant laws and regulations"},
+    {"name": "Branded Company File",
+        "description": "As a partner, I want a branded and customized company file for potential investments"},
+    {"name": "Email Templates",
+        "description": "As an admin, I want email templates for all stages of contact for both VDs and companies"},
+    {"name": "Dashboard Tips", "description": "As a user, I want the dashboard to provide tips for small changes that will increase matching probability"}
+]
+
+tasks = [
+    {"summary": "Define Registration Data Model",
+     "description": "Define the schema for a User in the database", "type": "Task"},
+    {"summary": "Create Registration API Endpoint",
+     "description": "Implement an API endpoint to handle user registration requests.", "type": "Task"},
+    {"summary": "Implement User Registration UI",
+     "description": "Design the registration form in Figma.", "type": "Task"},
+    {"summary": "Connect Registration UI to API",
+     "description": "Implement functionality to make requests from the registration form to the backend API.", "type": "Task"},
+    {"summary": "Implement Registration Confirmation Email",
+     "description": "Implement a feature to send a confirmation email after a successful registration.", "type": "Task"},
+    {"summary": "Testing and QA", "description": "Implement unit tests and integration tests for registration. Manual testing of the registration process.", "type": "Task"},
+]
+
+for epic_data in epics_data:
+    epic = create_issue(
+        project_key, epic_data["name"], epic_data["description"], "Epic", epic_name=epic_data["name"])
+
+    for task_data in tasks:
+        task = create_issue(
+            project_key, task_data["summary"], task_data["description"], task_data["type"], epic_link=epic.key)
